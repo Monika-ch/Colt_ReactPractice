c7019b124f2fcc06d10a372618dde128abaf376d
monika12b <monika12b@gmail.com>
2023-04-14 18:27:35 -0700

    Conditionals, App structure in JSX

---
+++ Practice/03_NumPicker: JSX_Conditionals/app.js
@@ -0,0 +1,32 @@
+function getNum() {
+  return Math.floor(Math.random() * 10) + 1;
+}
+
+class NumPicker extends React.Component {
+  render() {
+    const num = getNum();
+    let msg;
+    if (num === 7) {
+      msg = (
+        <div>
+          <h2>CONGRATS, YOU WIN!</h2>
+          <img src='https://media.tenor.com/RO4xfVyooqQAAAAi/cute.gif' />
+        </div>
+      );
+    } else {
+      msg = <p>Sorry, You Lose!</p>;
+    }
+    return (
+      <div>
+        <h1>Your number is {num}</h1>
+        {msg}
+        {/* <p>{num === 7 ? "Congrats! 🥳" : "Unlucky! 😵"}</p>
+        {num === 7 && (
+          <img src='https://media.tenor.com/RO4xfVyooqQAAAAi/cute.gif' />
+        )} */}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<NumPicker />, document.getElementById("root"));
