319d2b322d5113113e968d13ea7828e1d836b08c
monika12b <monika12b@gmail.com>
2023-05-09 17:59:19 -0700

    state intro and alternate syntax

---
+++ Practice/07_State_Demo/state_demo/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Game from "./Game";
+import Game2 from "./Game2";
+
+function App() {
+  return (
+    <div className='App'>
+      <Game />
+      <Game2 />
+    </div>
+  );
+}
+
+export default App;
