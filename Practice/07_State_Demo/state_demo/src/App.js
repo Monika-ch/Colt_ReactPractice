a892ba8b5680d551c0170d4e672d8030dc7c88bc
monika12b <monika12b@gmail.com>
2023-05-09 18:48:59 -0700

    setting state with setState()

--- Practice/07_State_Demo/state_demo/src/App.js
+++ Practice/07_State_Demo/state_demo/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Game from "./Game";
 import Game2 from "./Game2";
+import Rando from "./Rando";
 
 function App() {
   return (
     <div className='App'>
       <Game />
       <Game2 />
+      <Rando maxNum={7} />
     </div>
   );
 }
