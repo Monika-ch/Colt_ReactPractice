85fbc1f035047e7e866389c3d9747dd4d10839df
monika12b <monika12b@gmail.com>
2023-05-10 17:29:23 -0700

    click event intro

--- Practice/07_State_Demo/state_demo/src/App.js
+++ Practice/07_State_Demo/state_demo/src/App.js
@@ -1,14 +1,16 @@
 import "./App.css";
 import Game from "./Game";
 import Game2 from "./Game2";
-import Rando from "./Rando";
+// import Rando from "./Rando";
+import BrokenClick from "./BrokenClick";
 
 function App() {
   return (
     <div className='App'>
       <Game />
       <Game2 />
-      <Rando maxNum={7} />
+      {/* <Rando maxNum={7} /> */}
+      <BrokenClick />
     </div>
   );
 }
