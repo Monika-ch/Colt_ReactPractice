acb766a70e500bcc94bfb2fe6fffc4765ce19ce1
monika12b <monika12b@gmail.com>
2023-06-23 02:57:24 -0700

    make game winnable and styling

--- Projects/hangman/src/Hangman.js
+++ Projects/hangman/src/Hangman.js
@@ -64,8 +64,12 @@ class Hangman extends Component {
 
   /** render: render game */
   render() {
+    const isWinner = this.guessedWord().join("") === this.state.answer;
     const gameOver = this.state.nWrong >= this.props.maxWrong;
     const altText = `${this.state.nWrong}/${this.props.maxWrong} guesses`;
+    let gameState = this.generateButtons();
+    if (isWinner) gameState = "You Win!";
+    if (gameOver) gameState = "You Lose!";
     return (
       <div className='Hangman'>
         <h1>Hangman</h1>
@@ -74,10 +78,10 @@ class Hangman extends Component {
         <p className='Hangman-word'>
           {!gameOver ? this.guessedWord() : this.state.answer}
         </p>
-        <p className='Hangman-btns'>
-          {!gameOver ? this.generateButtons() : `You lose!`}
-        </p>
-        <button onClick={this.reset}>Restart?</button>
+        <p className='Hangman-btns'>{gameState}</p>
+        <button id='reset' onClick={this.reset}>
+          Restart?
+        </button>
       </div>
     );
   }
