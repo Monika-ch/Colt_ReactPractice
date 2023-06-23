33e0da8bcf3efedb2150054e32c88e0ce9859ce3
monika12b <monika12b@gmail.com>
2023-06-23 00:18:38 -0700

    passing methods to child (not inside parent render method)

--- Practice/09_React_Events/number_list/src/App.js
+++ Practice/09_React_Events/number_list/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import NumberList from "./NumberList";
+import BetterNumberList from "./BetterNumberList";
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <NumberList />
+      <BetterNumberList />
     </div>
   );
 }
