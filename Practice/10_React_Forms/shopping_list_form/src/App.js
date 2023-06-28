91964710d86fb750c1a73fac3531fd3a70069322
monika12b <monika12b@gmail.com>
2023-06-27 17:31:18 -0700

    form- shopping list (design pattern: passing data upwards)

---
+++ Practice/10_React_Forms/shopping_list_form/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import MultipleForm from "./ShoppingList";
+
+function App() {
+  return (
+    <div className='App'>
+      <MultipleForm />
+    </div>
+  );
+}
+
+export default App;
